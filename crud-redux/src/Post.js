2e06a651e383795632abbc34e6ff365e87869d08
Kiara Contreras <kiara@hashtag-labs.com>
2018-09-18 17:35:05 -0400

    delete working

--- crud-redux/src/Post.js
+++ crud-redux/src/Post.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Post extends Component {
     render() {
@@ -6,9 +7,11 @@ class Post extends Component {
             <div>
                 <h2>{this.props.post.title}</h2>
                 <p>{this.props.post.message}</p>
+                <button>Edit</button>
+                <button onClick={()=> this.props.dispatch({type:'DELETE_POST', id:this.props.post.id})}>Delete</button>
             </div>
             )
     }
 }
 
-export default Post;
+export default connect()(Post);
